f9f741d15e3affc79d87e0e756ecefb05e305537
Mariia Kolo <marikolo57@gmail.com>
2026-04-21 14:04:02 +0100

    implemented current user route

---
+++ src/app/api/authorisation/me/route.js
@@ -0,0 +1,54 @@
+//Mariia Kolodiazhna 3149166
+import { NextResponse } from 'next/server';
+import { getSession } from '@/lib/session';
+import pool from '@/lib/db';
+
+export async function GET(request) {
+  try {
+    const sessionId = request.cookies.get('session_id')?.value;
+    
+    //if no responding cookie, user is not logged in
+    if (!sessionId) {
+      return NextResponse.json(
+        { error: 'Not authenticated' },
+        { status: 401 }
+      );
+    }
+
+    //validtae session
+    const session = getSession(sessionId);
+    
+    //if session expired reject requet
+    if (!session) {
+      return NextResponse.json(
+        { error: 'Invalid or expired session' },
+        { status: 401 }
+      );
+    }
+
+    //fetch user data from database
+    const [users] = await pool.query(
+      'SELECT user_id, first_name, last_name, email, phone, role, created_at FROM users WHERE user_id = ?',
+      [session.user_id]
+    );
+
+    //check user exists
+    if (users.length === 0) {
+      return NextResponse.json(
+        { error: 'User not found' },
+        { status: 404 }
+      );
+    }
+
+    //send data to frontend
+    return NextResponse.json({ user: users[0] });
+
+    //handle possible errors
+  } catch (error) {
+    console.error('Get user error:', error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
