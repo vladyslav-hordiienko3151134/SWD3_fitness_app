ceb0b351cbf86e00adb0f36817dcbd22cc6475b9
Ivan <ivanspinko@gmail.com>
2026-05-01 18:29:25 +0100

    code refactoring

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -29,7 +29,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div class = "class">
+    <div className="class">
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
         <div>
